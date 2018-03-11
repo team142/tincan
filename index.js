5d1eea89bdff6ca8b1e288b60f90ad2c90931d75
earv1 <emmanuelarvanitis@gmail.com>
2018-03-11 17:58:06 +0200

    First Commit, prototype

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+exports.__esModule = true;
+var server = require('http').createServer(function incoming(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    require('fs').createReadStream(__dirname + '/index.html').pipe(res);
+});
+//
+// Attach Primus to the HTTP server.
+//
+var Primus = require('primus');
+var primus = new Primus(server);
+//
+// Listen for connections and echo the events send.
+//
+var connections = [];
+primus.on('connection', function connection(spark) {
+    connections.push(spark);
+    spark.on('data', function received(data) {
+        console.log(spark.id, 'received message:', data);
+        for (var _i = 0, connections_1 = connections; _i < connections_1.length; _i++) {
+            var connection_1 = connections_1[_i];
+            connection_1.write(data);
+        }
+    });
+});
+server.listen(8080, function () {
+    console.log('Open http://localhost:8080 in your browser');
+});
